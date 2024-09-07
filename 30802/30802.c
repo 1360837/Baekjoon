2587c5eb9b4a420cfe031b55e42a3ce24092bed3
1360837 <woowoo4599@gmail.com>
2024-09-07 15:37:43 +0900

    0

---
+++ 30802/30802.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	unsigned int	N, T, P;
+	unsigned int	size[6] = {0};
+	unsigned int	cnt = 0;
+
+	scanf("%d", &N);
+	for (int i = 0; i < 6; i++)
+		scanf("%d", &size[i]);
+	scanf("%d %d", &T, &P);
+
+	for (int i = 0; i < 6; i++)
+	{
+		if (size[i] % T)
+			cnt++;
+		cnt += (size[i] / T);
+	}
+	printf("%d\n", cnt);
+	printf("%d %d\n", N / P, N % P);
+}
