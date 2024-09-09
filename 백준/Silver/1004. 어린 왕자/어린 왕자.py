629c2041c817425e7a7bb0f18dc5049c6b9b66fc
JiWoo Nam <129957447+1360837@users.noreply.github.com>
2024-09-09 11:42:19 +0900

    [Silver III] Title: 어린 왕자, Time: 236 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -0,0 +1,11 @@
+T=int(input())
+
+for _ in range(T):
+  count=0
+  x1,y1,x2,y2=map(int,input().split())
+  n=int(input())
+  for i in range (n):
+    cx,cy,r=map(int,input().split())
+    if (((cx-x1)**2 + (cy-y1)**2)<r**2) or (((cx-x2)**2 + (cy-y2)**2)<r**2) : count+=1
+    if (((cx-x1)**2 + (cy-y1)**2)<r**2) and (((cx-x2)**2 + (cy-y2)**2)<r**2) : count-=1
+  print(count)
