a7fef69acd488ef7dfe31651585d56b0da88d711
JiWoo Nam <129957447+1360837@users.noreply.github.com>
2024-09-27 15:46:41 +0900

    [Silver III] Title: 평행 우주, Time: 64 ms, Memory: 2284 KB -BaekjoonHub

--- 백준/Silver/17451. 평행 우주/평행 우주.c
+++ 백준/Silver/17451. 평행 우주/평행 우주.c
@@ -1,30 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int n;
-int arr[300001];
+int	main(void)
+{
+	int	n, *v;
+	long long	need_v;
 
-int main(void) {
-    // n 입력 받기
-    scanf("%d", &n);
-
-    // 배열 입력 받기
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    // res에 마지막 값 저장
-    long long res = arr[n - 1];
-
-    // 뒤에서 두번째부터 역순으로 계산
-    for (int i = n - 2; i >= 0; i--) {
-        if (res % arr[i] == 0)
-            continue; // 이미 배수면 그대로 넘어감
-        long long m = res / arr[i] + 1; // 배수가 되도록 업데이트
-        res = arr[i] * m;
-    }
-
-    // 결과 출력
-    printf("%lld\n", res);
-
-    return 0;
+	scanf("%d", &n);
+	v = malloc(sizeof(int) * n);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &v[i]);
+	need_v = v[n - 1];
+	for (int i = n -2; i >= 0; i--)
+	{
+		if (need_v % v[i] == 0)
+			continue ;
+		need_v = (long long)(need_v / v[i] + 1) * v[i];
+	}
+	printf("%lld\n", need_v);
+	free(v);
+	return 0;
 }
